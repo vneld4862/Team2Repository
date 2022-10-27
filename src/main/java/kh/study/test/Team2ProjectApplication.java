f2a892e5f2b7984104fe58a70500ca3e9a1210df
vneld4862 <vneld4862@gmail.com>
2022-10-27 12:28:19 +0900

    프로젝트 생성

---
+++ src/main/java/kh/study/test/Team2ProjectApplication.java
@@ -0,0 +1,13 @@
+package kh.study.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team2ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team2ProjectApplication.class, args);
+	}
+
+}
